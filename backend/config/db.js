8c094334fa87f839ce8601bba45fad9b57b3468c
Ishan Rawat <ishanrawat4@gmail.com>
2024-04-16 18:45:46 +0530

    Added the DB String and Stripe Secret Key

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://TastyBiteHub:TastyBiteHub@cluster0.zenrymt.mongodb.net/food-del').then(()=>console.log("DB Connected"))
 }
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
